6cd679de20529c08c2bdf7156eaa8c5afca46d03
drishty11 <drishtypal1107@gmail.com>
2021-06-19 15:42:00 +0530

    comment

--- src/Components/Footer/footer.js
+++ src/Components/Footer/footer.js
@@ -1,7 +1,10 @@
 import React from 'react';
  const Footer =()=>{
+     
+     //this is a footer
+
  return(   
-        <nav className="navbar navbar-default  navbar-fixed-bottom" role="navigation" style={{background: 'linear-gradient(179.88deg, #011B53 -143.82%, #063DB5 226.52%)',
+        <nav className="navbar navbar-default navbar-fixed-bottom" role="navigation" style={{background: 'linear-gradient(179.88deg, #011B53 -143.82%, #063DB5 226.52%)',
         color: 'rgba(255, 250, 240, 1)'}}>
     <div className="container text-center">
         <p className="navbar-text col-md-12 col-sm-12 col-xs-12">All rights are reserved</p>
